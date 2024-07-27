3e873e1a1233a76d38ae853d13fc50c954bbebea
emilysm1209 <esteyermader@ufl.edu>
2024-07-27 18:55:34 -0400

    adding encode function

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ def decode(password):
     decoded_password = ''.join(str((int(char) - 3) % 10) for char in password)
     return decoded_password
 
-print("hi")
+def encode(password):
+    encoded_password = ''.join(str(int(char) + 3) % 10 for char in password)
+    return encoded_password
